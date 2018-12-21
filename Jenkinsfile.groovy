dc6fca118b6e916a223ad70768d24e8386cb69a8
Serhii Dovhan <serhii.dovhan@gmail.com>
2018-12-21 18:10:56 +0200

    save

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,12 +9,16 @@ pipeline {
         booleanParam defaultValue: true, description: '', name: 'skipTests'
         string defaultValue: 'tags/jenkins-2.153', description: '', name: 'branch', trim: true
     }
-    
+
     stages {
-        stage ('Build') {
+        stage ('Info') {
             steps {
-                sh 'echo "Hello!"'
-            }
+                sh 'echo "skipTests = ${skipTests}"'
+                sh 'echo "branch = ${branch}"'
+            }            
+        }
+        stage ('Build') {
+
         }
     }
 
