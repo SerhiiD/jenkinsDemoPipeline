f753198c650dd4014bb8a89416fb9db3a17da58c
Serhii Dovhan <serhii.dovhan@gmail.com>
2018-12-21 19:07:37 +0200

    save

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,6 +17,7 @@ pipeline {
                 sh 'echo "branch = ${branch}"'
                 library 'myLib'
                 nothing()
+                nothing.sayHello()
             }            
         }
 
