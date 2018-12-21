5a6d72186e60d7380ba19d87e74ccf148f68be5b
Serhii Dovhan <serhii.dovhan@gmail.com>
2018-12-21 18:09:02 +0200

    save

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,8 +5,13 @@ pipeline {
         maven 'M3'
     }
 
+    parameters {
+        booleanParam defaultValue: true, description: '', name: 'skipTests'
+        string defaultValue: 'tags/jenkins-2.153', description: '', name: 'branch', trim: true
+    }
+    
     stages {
-        stage ('Build'){
+        stage ('Build') {
             steps {
                 sh 'echo "Hello!"'
             }
